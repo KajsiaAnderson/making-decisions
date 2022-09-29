26c5f60f08ea292ef4c25fe65d438cf8a4b4df02
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-09-28 21:46:09 -0600

    finished problems 6 to 11

--- lab.js
+++ lab.js
@@ -77,8 +77,8 @@ if (temperature >= 80 && rain) {
 */
 
 //CODE HERE
-for(let hello; hello < 10; hello++){
-  console.log(hello)
+for(let i=0; i < 10; i++){
+  console.log("hello")
 }
 
 ////////// PROBLEM 6 //////////
@@ -88,7 +88,7 @@ for(let hello; hello < 10; hello++){
 */
 
 //CODE HERE
-for(let i = 1; i < 10; i++){
+for(let i = 1; i < 11; i++){
   console.log(i)
 }
 
@@ -99,7 +99,9 @@ for(let i = 1; i < 10; i++){
 */
 
 //CODE HERE
-
+for(let i = 10; i > 0; i--){
+  console.log(i)
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -111,6 +113,10 @@ let passingScore = 7;
 */
 
 //CODE HERE
+while (score < passingScore){
+  console.log("your score is not high enough")
+  score ++
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
@@ -122,11 +128,21 @@ let passingScore = 7;
 */
 
 //CODE HERE
+let changeMyMind = true
+
+if (changeMyMind){
+  console.log(changeMyMind = false)
+} else {
+  console.log(changeMyMind = true)
+}
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+changeMyMind = !changeMyMind
+console.log(changeMyMind)
+
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -136,3 +152,8 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+while(z > 0){
+  for(let i = z; i > 0; i--) {
+  console.log(i)
+} z--
+}
